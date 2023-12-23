c31b3fec5d5949aae08215d5bdb1f01192e07ec0
Bhuban Mohan Patra <patrabhubanmohan@gmail.com>
2023-12-23 08:54:10 +0530

    orientation change

--- pair.cpp
+++ pair.cpp
@@ -3,16 +3,16 @@ using namespace std;
 
 int main() {
     pair <int , int> p = {2,4};
-    cout<<p.first<<" "<<p.second<<endl;
+        cout<<p.first<<" "<<p.second<<endl;
     pair<int,char> a;
-    a.first = 2;
-    a.first = 56;
-    a.second = 'c';
-    cout<<a.first<<" "<<a.second<<endl;
+        a.first = 2;
+        a.first = 56;
+        a.second = 'c';
+        cout<<a.first<<" "<<a.second<<endl;
    pair<int ,pair<int,int>> t={3,{4,6}};
-   cout<<t.first<<" "<<t.second.first<<" "<<t.second.second<<endl;
+        cout<<t.first<<" "<<t.second.first<<" "<<t.second.second<<endl;
    pair<int,int>arr[] = { {3,4},{6,67}};
-   cout<<arr[1].second<<endl;
+        cout<<arr[1].second<<endl;
 
     return 0;
 }
