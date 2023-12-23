0886058a7d1b84408ffbf204453487aa180b6b2e
Bhuban Mohan Patra <patrabhubanmohan@gmail.com>
2023-12-23 10:14:30 +0530

    time complexity added

--- stl c++/list and Deque.cpp
+++ stl c++/list and Deque.cpp
@@ -1,9 +1,12 @@
+#include<bits/stdc++.h>
+   using namespace std;
 
+   int mian(){
    // similar with vector (deque = vector, list = vector + front operation)
    // push/pop_back, insert, swap , erase etc    +   front operation
    
    list<int> ls;
-   ls.push_front(34);
+   ls.push_front(34);         //O(n)
    ls.push_front(6);
    ls.push_front(567);
    ls.push_front(8654);
@@ -12,3 +15,6 @@
    
    for(auto i:ls)
     cout<<i<<" ";
+
+   return 0;
+   }
--- stl c++/vector.cpp
+++ stl c++/vector.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     vector<int> v;
-    v.push_back(4);
+    v.push_back(4);                                      //O(1)
     v.push_back(7);
     v.push_back(3);
     v.push_back(9);
@@ -11,33 +11,33 @@ int main() {
     v.push_back(3);
     v.pop_back();
     
-    // cout<<v.size()<<endl;
+    // cout<<v.size()<<endl;                  //O(1)
     
     // cout<<v[2]<<" "<<v.at(1)<<endl;
     
-    // cout<<v.front()<<" "<<v.back()<<endl;
+    // cout<<v.front()<<" "<<v.back()<<endl;                   //O(1)
     
     
     //erase function ------------------------------------------------
     // v.erase(v.begin()+1);
-    // v.erase(v.begin()+1,v.begin()+3);
+    // v.erase(v.begin()+1,v.begin()+3);      //O(n)
     // v.erase(v.begin()+1,v.end()-2);
     
     //insert function --------------------------------------------------
     // v.insert(v.begin(),455);
-    // v.insert(v.begin()+1,3,100);
+    // v.insert(v.begin()+1,3,100);         //O(n)
     
     //swap function ----------------------------------------------------
     // vector<int> v1 ={3,5,7};
-    // v.swap(v1);
+    // v.swap(v1);                         // swapping the internal pointers to the memory used by the vectors, rather than copying the entire contents           //O(1)
     //     for(auto i : v1)
     //         cout<<i<<" ";
     //     cout<<endl;
     
     
-    cout<<v.empty()<<endl;   // return in boolean
+    cout<<v.empty()<<endl;   // return in boolean     //O(1)
     
-    v.clear();        // remove all elements
+    v.clear();        // remove all elements            //O(1)
     cout<<v.size()<<endl;
     
     // for(vector<int>::iterator it = v.begin();it!=v.end();it++)
